dce3f9b973f9a865dc1ed92c85fda3d27313651e
55011 <550112278@qq.com>
2020-12-01 17:13:06 +0800

    这是ccc的auth项目代码

---
+++ src/main/java/com/bjpowernode/auth/entity/Role.java
@@ -0,0 +1,43 @@
+package com.bjpowernode.auth.entity;
+
+public class Role {
+    private Integer roleId;
+
+    private String roleName;
+
+    private String roleCode;
+
+    private Integer roleStatus;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+
+    public Integer getRoleStatus() {
+        return roleStatus;
+    }
+
+    public void setRoleStatus(Integer roleStatus) {
+        this.roleStatus = roleStatus;
+    }
+}
---
+++ src/main/java/com/bjpowernode/auth/entity/User.java
@@ -0,0 +1,53 @@
+package com.bjpowernode.auth.entity;
+
+public class User {
+    private Integer userId;
+
+    private String userName;
+
+    private String userPwd;
+
+    private Integer userSex;
+
+    private Integer userAge;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+    public Integer getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(Integer userSex) {
+        this.userSex = userSex;
+    }
+
+    public Integer getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(Integer userAge) {
+        this.userAge = userAge;
+    }
+}
