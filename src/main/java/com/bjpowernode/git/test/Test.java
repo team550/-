eea180e8ed24430040653f3d5648b32144390339
55011 <550112278@qq.com>
2020-12-01 13:55:53 +0800

    这是ccc

---
+++ src/main/java/com/bjpowernode/git/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * @ClassName test
+ * @Description 一句话描述此类或方法的作用
+ * @Author PowerNode
+ * @Date 2020/12/1 13:53
+ * @Version 1.0
+ */
+package com.bjpowernode.git.test;
+/** 
+ *@ClassName Test
+ *@Description 一句话描述此类或方法的作用
+ *@Author PowerNode
+ *@Date 2020/12/1 13:55
+ *@Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是git");
+    }
+}
